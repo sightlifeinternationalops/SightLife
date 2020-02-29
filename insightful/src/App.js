e876eae165b8ac1d8c7073994519d17b19f1f0ca
magdanat <nathanmagdalera@gmail.com>
2020-02-29 12:18:38 -0800

    make changes for layout

--- insightful/src/App.js
+++ insightful/src/App.js
@@ -11,44 +11,58 @@ import './css/App.css';
 
 import SightLife from './img/sightlife.png';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <nav className="wrapper navbar navbar-default">
-           <img className = "logo" src={SightLife} style={{width:100, flex: 1, flexDirection: 'row', paddingRight: 890, paddingTop:10}} />
+      // <Router>
+      //   <div>
+      //     <nav className="wrapper navbar navbar-default">
+      //      <img className = "logo" src={SightLife} style={{width:100, flex: 1, flexDirection: 'row', paddingRight: 890, paddingTop:10}} />
 
-            <Link to="/" className="link"><strong>About</strong></Link>
-            {' '}
-            <Link to="/Metrics" className="link"><strong> Metrics </strong></Link>
-            {' '}
-            <Link to="/HistoricalData" className="link"><strong>Historical Data</strong></Link>
-            {' '}
-            <Link to="/DataEntry" className="link"><strong> Data Entry </strong></Link>
-            {' '}
-            <Link to="/FAQ" className="link"><strong> FAQ </strong></Link>
-            {' '}
-            <Link to="/SignIn" className='link'> <strong> Sign In </strong> </Link> 
-            {' '}
-          </nav>
+      //       <Link to="/" className="link"><strong>About</strong></Link>
+      //       {' '}
+      //       <Link to="/Metrics" className="link"><strong> Metrics </strong></Link>
+      //       {' '}
+      //       <Link to="/HistoricalData" className="link"><strong>Historical Data</strong></Link>
+      //       {' '}
+      //       <Link to="/DataEntry" className="link"><strong> Data Entry </strong></Link>
+      //       {' '}
+      //       <Link to="/FAQ" className="link"><strong> FAQ </strong></Link>
+      //       {' '}
+      //       <Link to="/SignIn" className='link'> <strong> Sign In </strong> </Link> 
+      //       {' '}
+      //     </nav>
 
-          <Route exact path="/" component={About} />
-          <Route path="/HistoricalData" component={HistoricalData} />
-          <Route path="/Metrics" component={Metrics} />
-          <Route exact path="/DataEntry" component={DataEntry} />
-          <Route path="/FAQ" component={FAQ} />
-          <Route path='/SignIn' component={SignIn} />
+      //     <Route exact path="/" component={About} />
+      //     <Route path="/HistoricalData" component={HistoricalData} />
+      //     <Route path="/Metrics" component={Metrics} />
+      //     <Route exact path="/DataEntry" component={DataEntry} />
+      //     <Route path="/FAQ" component={FAQ} />
+      //     <Route path='/SignIn' component={SignIn} />
 
-          <footer>
-            <div className="footer-container">
-              <p className="inSightful Footer"> This project is a part of the : </p>
-              <a className="Data" href="https://ischool.uw.edu/capstone"> Capstone Project course at the University of Washington Information School </a>
-            </div>
-          </footer>
+      //     <footer>
+      //       <div className="footer-container">
+      //         <p className="inSightful Footer"> This project is a part of the : </p>
+      //         <a className="Data" href="https://ischool.uw.edu/capstone"> Capstone Project course at the University of Washington Information School </a>
+      //       </div>
+      //     </footer>
 
-        </div>
-      </Router>
+      //   </div>
+      // </Router>
+      <Navbar/>
+    );
+
+  }
+}
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <p>test</p>
+      </div>
     );
   }
 }
+
+export default App;
