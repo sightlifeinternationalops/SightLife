496516726abd9c98c40053e8f2a9d0b95dfcf31d
magdanat <nathanmagdalera@gmail.com>
2020-03-04 23:28:23 -0800

    Add function for MetricAreaCard

--- insightful/src/Metrics.js
+++ insightful/src/Metrics.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Switch, Route, Link} from 'react-router-dom';
-import { Button, ButtonGroup, ButtonToolbar} from 'reactstrap';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Button, ButtonGroup, ButtonToolbar } from 'reactstrap';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardDeck, CardGroup } from 'reactstrap';
 import './css/Metrics.css';
 import './index.js';
@@ -19,42 +19,45 @@ export class Metrics extends Component {
         }
     }
 
-  // Callback for rendering metric calculations in the dashboard page.
-  getMetricCalculations = (routerProps) => {
-    let rootPath = firebase.database().ref('metricCalculations')
-    rootPath.once('value', (snapshot) => {
-        let metricCalculationInfo = snapshot.val();
-        // check the metricAreaID of every metric calculation, if that metricAreaID is the same as the one we want
-        // then add it to the list. 
-        let databaseKeys = Object.keys(metricCalculationInfo);
-        databaseKeys.map((key) => {
-            let metricCalcPath = firebase.database().ref('metricCalculations/' + key).child("metricAreaID")
-            metricCalcPath.once('value', (snapshot) => {
-                let info = snapshot.val();
-                // if info is equal to target metricAreaID, then 
-                // information needed for dashboard...
-                // 1. Metric Calculations
-                // 2. Owner of Metric Area
-                // 3. Metric Calculations on a month by month, quarter by quarter, and year by year basis.
-                console.log(info);
+    // Callback for rendering metric calculations in the dashboard page.
+    getMetricCalculations = (routerProps) => {
+        let rootPath = firebase.database().ref('metricCalculations')
+        rootPath.once('value', (snapshot) => {
+            let metricCalculationInfo = snapshot.val();
+            // check the metricAreaID of every metric calculation, if that metricAreaID is the same as the one we want
+            // then add it to the list. 
+            let databaseKeys = Object.keys(metricCalculationInfo);
+            databaseKeys.map((key) => {
+                let metricCalcPath = firebase.database().ref('metricCalculations/' + key).child("metricAreaID")
+                metricCalcPath.once('value', (snapshot) => {
+                    let info = snapshot.val();
+                    // if info is equal to target metricAreaID, then 
+                    // information needed for dashboard...
+                    // 1. Metric Calculations
+                    // 2. Owner of Metric Area
+                    // 3. Metric Calculations on a month by month, quarter by quarter, and year by year basis.
+                    console.log(info);
+                    if (info = this.props.metricAreaID) {
+
+                    }
+                })
             })
         })
-    })
-}
+    }
 
     render() {
-        return(
+        return (
             // Eventually need to pass in metric values as props
             <Switch>
-                <Route path="/Metrics/:metricID" render={(props) => <DashBoard {...props} 
-                                                                    metricAreaInfo={this.state.metricAreaInfo}/> } />
+                <Route path="/Metrics/:metricID" render={(props) => <DashBoard {...props}
+                    metricAreaInfo={this.state.metricAreaInfo} />} />
                 <div>
                     {
                         this.props.metrics.map((item) => {
                             // Pass metricName, metricID into metricAreaCard as props then also pass in a list of props containing information about that specific metric
-                            return <MetricAreaCard 
-                                    metricName={item}
-                                    />
+                            return <MetricAreaCard
+                                metricName={item}
+                            />
                         })
                     }
                 </div>
@@ -71,11 +74,16 @@ class MetricAreaCard extends Component {
         super(props);
     }
 
+    // To do. What does a spread operator look like?
+    setMetricID() {
+        console.log("Button was clicked!");
+    }
+
     render() {
         return (
             // When a link is clicked, retrieve the necessary information from firebase and then put it into metricAreaInfo
             <div>
-                <Link to={'/Metrics/' + this.props.metricName}>{this.props.metricName}</Link>
+                <Link to={'/Metrics/' + this.props.metricName} onClick={this.setMetricID}>{this.props.metricName}</Link>
             </div>
         )
     }
