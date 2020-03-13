f21887a9852d043e158153b73ac1cb36f274e681
magdanat <nathanmagdalera@gmail.com>
2020-03-13 12:50:28 -0700

    Reformat files

--- README.md
+++ README.md
@@ -1,32 +1,68 @@
-# InSightful
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Description
+## Available Scripts
 
+In the project directory, you can run:
 
-This repository holds information and code for SightLife's Global Score Card, a tool designed to help the organization understand metrics regarding their performance amongst different regions around the world. This project was developed by Team InSightful in accordance with their Informatics Capstone Project in 2020.
+### `npm start`
 
-The Global Score Card will operate as a dashboard, creating data visualizations based upon inputted data to help SightLife see trends, compare the actual metrics to their target metrics, and improve upon their current methods of tracking information.
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-## Instructions
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
 
-### Building
+### `npm test`
 
-**In Progress**
+Launches the test runner in the interactive watch mode.<br />
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.<br />
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.<br />
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+
+### Analyzing the Bundle Size
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
+
+### Making a Progressive Web App
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
+
+### Advanced Configuration
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
 
 ### Deployment
 
-**In Progress**
+This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
 
-## Contributions
+### `npm run build` fails to minify
 
-**In Progress**
-
-## Contact Information
-
-Role | Name | Email
------------- | ------------- | -------------
-Project Manager / User Experience | Shruti Rajagopalan | shrutir@uw.edu
-Back-End Developer /  User Experience | Alexander Escalera | aoes@uw.edu
-Full-Stack Developer | Nathan Magdalera | magdanat@uw.edu
-Data Science | Rani Chang | yungtc22@uw.edu
-
+This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
---
+++ src/About.js
@@ -0,0 +1,188 @@
+import './index.js';
+import './css/About.css';
+import React, { Component } from 'react';
+import {Card, CardImg, CardText, CardBody, CardTitle, CardDeck, CardGroup } from 'reactstrap';
+import { SocialIcon } from 'react-social-icons';
+import { Button, ButtonToolbar} from 'reactstrap';
+
+import Shruti from './img/shruti.png';
+import Alex from './img/alex.jpg';
+import Nathan from './img/nathan.jpg';
+import Rani from './img/rani.jpg';
+
+import Development from './img/development.png';
+import Policy from './img/policy.png';
+import Prevention from './img/prevention.png';
+import Training from './img/training.png';
+
+export class About extends Component {
+    render() {
+        return(
+        <div> 
+            <section class = "wave">
+                <div id="title" class="section-content">
+                    <div>
+                        <h1>SightLife Global Scorecard</h1>
+                        <p class = "intro">
+                            An international operations scorecard for SightLife to track projects, programs and 
+                            departments with a mission to eradicate corneal blindness by 2040
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+            <section id="problem">
+                <div class="section-content">
+                    <h2 class = "problem">The Problem</h2>
+                    <p class="problem">
+                        Right now, in this instance, there are more than <strong>12.7 million people in the world </strong> that are needlessly suffering from corneal blindness. 
+                        Corneal blindness is the scarring of the cornea caused by an injury or disease. Damage to the cornea is more common than we think, 
+                        but with SightLife, those suffering may all be relieved by 2040. <strong> SightLife’s main goal is to eliminate this life-damaging ailment </strong>. 
+                    </p>
+
+                    <p class = "problem">
+                        But with their current system of organizing/visualizing data, it is a hindrance to their overall goal. 
+                        This is why the global scorecard is a must-have. With this scorecard, it allows regional leads to have a platform where they can keep track of progress made 
+                        tied to each region around the world where they are currently working to eradicate corneal blindness. 
+                        This scorecard will provide visualizations that show trends over time, address different success metrics, 
+                        and contain customizable dashboards for users from various departments
+                    </p>
+                </div>
+            </section>
+
+            <section>
+                <div class="section-content">
+                    <div class="flex-container">
+                        <div class="flex-item">
+                            <h2>Sponsor Information</h2>
+                            <p>
+                                This project is sponsored by SightLife, the only global health organization working relentlessly to eliminate corneal blindness worldwide. With
+                                partners in more than 30 countries, SightLife is increasing
+                                the number of corneal transplants performed each year to
+                                transform millions of lives around the globe.
+                            </p>
+
+                            <p class='learnMore'>
+                                <a href="https://www.sightlife.org/">Learn more > </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="departments">
+                <div class="section-content">
+                    <h2>
+                        Departments
+                    </h2>
+                    <CardDeck> 
+
+                    <Card style={{ width: '18rem' }}>
+                        <CardImg top width="60%" src={ Development } alt='Development' />
+                            <CardBody>
+                            <CardTitle> <h3> Eye Bank Development </h3> </CardTitle>
+                                <CardText>
+                                    <p> Bringing industry-leading best practices to eye banks worldwide. </p>
+                                </CardText>
+                            </CardBody>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }}>
+                        <CardImg top width="64%" src={ Policy } alt='Policy' />
+                            <CardBody>
+                                <CardTitle body className="text-center"> <h3> Advocacy & Policy  </h3> </CardTitle>
+                                <CardText>
+                                <p> Creating policies that allow for more access to corneal tissue. </p>
+                                </CardText>
+                            </CardBody>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }}>
+                        <CardImg  top width="70%"src={ Prevention } alt='Prevention' />
+                            <CardBody>
+                                <CardTitle body className="text-center"> <h3> Prevention </h3> </CardTitle>
+                                <CardText>
+                                    <p> Preventing blindness and strengthening health systems with a simple intervention.</p>
+                                </CardText>
+                            </CardBody>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }}>
+                        <CardImg top width="64%" src={ Training } alt='Training' />
+                            <CardBody>
+                                <CardTitle body className="text-center"> <h3> Clinical Training </h3> </CardTitle>
+                                <CardText>
+                                    <p> Training trasplant surgeons and care providers worldwide. </p>
+                                </CardText>
+                            </CardBody>
+                        </Card>
+                    </CardDeck>
+                    </div>
+            </section>
+
+            <section id="contact">
+                <div class="section-content">
+                    <h2>
+                        Team Members & Contact Info
+                    </h2>
+                    
+                    <CardDeck> 
+
+                    <Card style={{ width: '18rem' }}>
+                        <CardImg class="card-img-top img-fluid" src={ Nathan } alt='Nathan' />
+                            <CardBody>
+                            <CardTitle> <h3> Nathan Magdalera <SocialIcon url="https://www.linkedin.com/in/nathanmagdalera/" style={{ height: 30, width: 30 }}/> </h3> </CardTitle>
+                                <CardText>
+                                    <p> Full-Stack Developer </p>
+                                    <p> Cybersecurity </p>
+                                    <p> magdanat@uw.edu </p>
+                                </CardText>
+                            </CardBody>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }}>
+                        <CardImg class="card-img-top img-fluid" src={ Shruti } alt='Shruti' />
+                            <CardBody>
+                                <CardTitle body className="text-center"> <h3> Shruti Rajagopalan <SocialIcon url="https://www.linkedin.com/in/shrutira/" style={{ height: 30, width: 30 }}/> </h3> </CardTitle>
+                                <CardText>
+                                    <p> User Experience</p>
+                                    <p>Project Management </p>
+                                    <p> shrutir@uw.edu </p>
+                                </CardText>
+                            </CardBody>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }}>
+                        <CardImg class="card-img-top img-fluid" src={ Alex } alt='Alex' />
+                            <CardBody>
+                                <CardTitle body className="text-center"> <h3>  Alexander Escalera  <SocialIcon url="https://www.linkedin.com/in/alexander-escalera-503360176/" style={{ height: 30, width: 30 }}/> </h3> </CardTitle>
+                                <CardText>
+                                    <p> Back-End Developer</p>
+                                    <p> User Experience </p>
+                                    <p> aoes@uw.edu </p>
+                                </CardText>
+                            </CardBody>
+                        </Card>
+
+                        <Card style={{ width: '18rem' }}>
+                        <CardImg class="card-img-top img-fluid" src={ Rani } alt='Rani' />
+                            <CardBody>
+                                <CardTitle body className="text-center"> <h3> Rani Chang <SocialIcon url="https://www.linkedin.com/in/yungtc/" style={{ height: 30, width: 30 }} /> </h3> </CardTitle>
+                                <CardText>
+                                    <p> Data Science</p>
+                                    <p> Front-End Developer </p>
+                                    <p> yungtc22@uw.edu </p>
+                                </CardText>
+                            </CardBody>
+                        </Card>
+
+                    </CardDeck>
+
+
+                </div>
+            </section>
+        </div>
+
+        )
+    }
+}
